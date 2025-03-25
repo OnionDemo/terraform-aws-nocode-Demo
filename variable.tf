b19705c5c4e964201f37e7ec2418a033ec522073
Ting <ting@hashicorp.com>
2025-03-24 20:49:54 -0700

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region for EC2 deployment"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Tag name for EC2 instance"
+  type        = string
+  default     = "No-Code-EC2"
+}
